04e79e69cb234f38faee9c73d3474c47cb4216f9
olusesimrg <olusesimrgees@gmail.com>
2023-04-08 20:30:27 +0100

    script js added

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,1 +1,53 @@
-document.getElementById("")
+const hourEl = document.getElementById("hour");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+function updateTime(){
+
+let hour = new Date().getHours();
+let minutes = new Date().getMinutes();
+let seconds = new Date().getSeconds();
+let ampm = "Am"
+
+if (hour > 12){
+  hour = hour - 12;
+  ampm = "PM"
+};
+
+hourEl.textContent = hour;
+minutesEl.textContent = minutes;
+secondsEl.textContent = seconds;
+ampmEl.textContent = ampm;
+
+if (hour < 10){
+  hourEl.textContent = "0" + hour;
+}else{
+  hourEl.textContent = hour;
+};
+
+// if time is single digit, add "0" else show time
+
+if (seconds < 10){
+  secondsEl.textContent = "0" + seconds;
+}else{
+  secondsEl.textContent = seconds;
+};
+// minutes
+if (minutes < 10){
+  minutesEl.textContent = "0" + minutes;
+}else{
+  minutesEl.textContent = minutes;
+};
+
+//console.log(hour + minutes)
+
+//minutes = minutes < 10 ? "0" + minutes : minutes;
+
+// call the updatetime() function every seconds
+setTimeout(() =>{ 
+  updateTime(); 
+}, 1000)
+
+};
+updateTime();
