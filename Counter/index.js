60c20d4fba474af34fc473a488ce91df93a8dcd6
olusesimrg <olusesimrgees@gmail.com>
2023-05-10 18:31:28 +0100

    counter done

--- Counter/index.js
+++ Counter/index.js
@@ -1,9 +1,36 @@
+
 const plus = document.getElementById("plus");
 const reset = document.getElementById("reset");
 const minus = document.getElementById("minus");
 const spanEl = document.getElementById("span");
+const btns = document.querySelectorAll(".btn");
 
 let num = 0;
+
+/*
+btns.forEach((btn) =>{
+     btn.addEventListener("click", (event) => {
+         const styles = event.currentTarget.classList;
+         if (styles.contains("bg-green-700")) {
+          num ++;
+         } else if (styles.contains("bg-red-700")) {
+          num --;
+         } else if (styles.contains("reset")){
+               num = 0;
+         }
+          spanEl.textContent = num;
+          
+          if(num > 0){
+               spanEl.style.color = "green"
+          } else if (num < 0){
+               spanEl.style.color = "red"
+          }else {
+               spanEl.style.color = "black"
+          }
+     });
+});
+
+*/
 
 //additional button
 plus.addEventListener("click", () =>{
@@ -34,6 +61,6 @@ minus.addEventListener("click", () =>{
      }else if(num == 0){
           spanEl.style.color = "black";
      }
-})
+});
 
 
